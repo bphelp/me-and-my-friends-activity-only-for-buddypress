1fa0bbfa688149caa11232360bca40f42bcf15b2
Ben Jones <icompugeek@gmail.com>
2013-06-21 05:22:19 -0400

    1.5

--- loader.php
+++ loader.php
@@ -1,14 +1,14 @@
 <?php 
 /*
 Plugin Name: Me And My Friends Activity Stream Only For BuddyPress
-Plugin URI: http://www.wordpress.com
+Plugin URI: https://github.com/bphelp/me-and-my-friends-activity-only-for-buddypress
 Description: Modifies the activity stream so you only see your friends and your own activities. Admins see all site activity. See readme.txt in the plugin file.
 Version: 1.5
 Requires at least: WordPress 3.2.1 / BuddyPress 1.5.1
 Tested up to: WordPress 3.6 beta2 / BuddyPress 1.8 beta1
 License: GNU/GPL 2
 Author: @bphelp
-Author URI: http://www.wordpress.com
+Author URI: http://bphelpblog.wordpress.com/
 */
 
 function bphelp_my_activity_privacy_init() {
